e1888f294b5253eff9aecdb4cbb00108b18378f6
Anil Eslavath <150352308+eslavathanilkumar@users.noreply.github.com>
2026-07-02 23:49:30 +0530

    Dockerized Student Management System

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()   # creates tables automatically
 
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
